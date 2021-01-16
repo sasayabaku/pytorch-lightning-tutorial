0c70d0bedd1d4a2b478b4924717f3d0b8e7908cd
sasayabaku <sasayabaku@yahoo.co.jp>
2021-01-17 00:08:53 +0900

    Create VGG19 customize Notebook

--- vgg19/model.py
+++ vgg19/model.py
@@ -23,7 +23,7 @@ class Vgg19Base(pl.LightningModule):
         self.valid_acc = pl.metrics.Accuracy()
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.9)
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.1, momentum=0.9)
         lambda_func = lambda epoch: 0.95 ** epoch
         lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_func)
 
@@ -36,27 +36,26 @@ class Vgg19Base(pl.LightningModule):
         loss = self.criterion(outputs, labels)
         self.train_acc(outputs, labels)
 
-        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
-        results = {'loss': loss}
+        return loss
 
-        return results
+    def training_epoch_end(self, outputs):
+        self.log('train_acc', self.train_acc.compute(), on_epoch=True, prog_bar=True, logger=True)
 
-    def training_step_end(self, outputs):
-        self.log('train_acc', self.train_acc.compute(), on_epoch=True, prog_bar=True)
-
-    def validation_step(self, batch, batch_nb):
+    def validation_step(self, batch, batch_idx):
         data, labels = batch
         outputs = self.forward(data)
 
         loss = self.criterion(outputs, labels)
         self.valid_acc(outputs, labels)
 
-        self.log('valid_acc', self.valid_acc, on_step=False, on_epoch=True, prog_bar=True)
-        self.log('valid_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('valid_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
-        results = {'loss': loss}
-        return results
+        return loss
+
+    def validation_epoch_end(self, outputs):
+        self.log('valid_acc', self.valid_acc.compute(), on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
 
 class VGG19Transfer(Vgg19Base):
@@ -83,3 +82,17 @@ class VGG19FineTuning(Vgg19Base):
     def __init__(self, n_class):
         super().__init__()
 
+        vgg_feature_extract_output_dim = self.vgg19.classifier[6].in_features
+        self.vgg19.classifier[6] = torch.nn.Linear(vgg_feature_extract_output_dim, n_class)
+
+        self.model = self.vgg19
+
+    def forward(self, x):
+        x = self.model(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.005, momentum=0.9)
+
+        return optimizer
